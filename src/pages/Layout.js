59f0771c36a361fce580b16d84229ad26581eccf
dawittamene <dawittamene559@gmail.com>
2024-01-03 03:54:30 +0300

    bulid nav bar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,24 +3,35 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav >
-        <ul className="b">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/testimonial">Testimonial</Link>
-          </li>
-        </ul>
-      </nav>
+      <nav className=" bg-slate-900 py-5 text-white justify-around flex">
+
+        <div>
+          <div>
+            
+          </div>
+          Zumbara Restaurant
+        </div>
+        <div className=" font-[popins] font-medium">
+          <ul className=" flex ml-2">
+            <li className=" ml-3">
+              <Link to="/">Home</Link>
+            </li>
+            <li className=" ml-3">
+              <Link to="/services">Services</Link>
+            </li>
+            <li className=" ml-3">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className=" ml-3">
+              <Link to="/testimonial">Testimonial</Link>
+            </li>
+          </ul>
+          </div>
+        </nav>
+     
 
       <Outlet />
+      
     </>
   )
 };
