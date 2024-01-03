f0162d2d2271dc08d9ce03408b4a4af9ce1fb938
dawittamene <dawittamene559@gmail.com>
2024-01-04 00:10:10 +0300

    add Banner page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import SampleServices from '../Components/SampleServices/SampleServices'
+import Banner from '../Components/Banner/Banner'
 
 const Home = () => {
   return (
     <section>
       <Hero />
       <SampleServices />
+      <Banner />
     </section>
   )
 }
